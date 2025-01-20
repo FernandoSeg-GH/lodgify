517434a1cd40bf3986b6b0ecc5f5aa0f06c517c0
FernandoSeg-GH <segre.fernando@gmail.com>
2025-01-20 18:44:26 +0100

    Jest Setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,15 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  testEnvironment: "jest-environment-jsdom",
-  testMatch: ["**/__tests__/**/*.test.ts?(x)"],
-  preset: "ts-jest",
+  preset: "ts-jest", // Use ts-jest to handle TypeScript
+  testEnvironment: "jsdom", // Provides DOM APIs like document for React components
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest", // Use ts-jest to process TS/TSX files
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1", // Match imports like @/components
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Set up testing-library
 };
 
 export default config;
